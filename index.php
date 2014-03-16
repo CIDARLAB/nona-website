148b48e965927a979a8d91216810885a7ab20aa6
fcwheat <fwheat@bu.edu>
2014-03-16 17:20:17 -0400

    init2

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="nona_favicon.png">
+    <link href='http://fonts.googleapis.com/css?family=Carrois+Gothic+SC' rel='stylesheet' type='text/css'>
+
+    <title>Nona</title>
+
+    <!-- Bootstrap core CSS -->
+    <script src="Jquery/jquery-1.11.0.min.js">
+</script>
+    <link href="Bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <script src = "Bootstrap/js/bootstrap.min.js"> </script>
+    <link href = "custom.css" rel = "stylesheet">
+    <script src = "content.js"> </script> 
+
+  </head>
+
+  <body style = "font-family: 'Carrois Gothic SC', sans-serif; color:black;">
+    <div class="container">
+    <div class="row">
+        <div class="col-xs-12" style = "padding-top:70px;">
+        <img  style = "display: block; margin-left:auto; margin-right:auto;width: 50%;" src = "Nona Foundation.png">
+    </div></div></div>
+<div class="row"><div class="col-xs-12" style = "padding-top:50px;">
+    <div style = "padding-right: 5px;"class = "navigation" id = "what"> What is Nona</div>
+    <div class = "navigation" id = "who"> Who is Nona</div>
+    <div class = "navigation" id = "projects"> Nona Projects</div>
+    <div class = "navigation" id = "contact"> Contact</div>
+    <div class = "navigation" id = "sponsors"> Sponsors</div>
+
+
+</div></div>
+<img src = "nav.png" style = "width:100px; height:5px; position:relative; left:0px; margin-top:-15px;" id = "nav">
+<div class="row"><div class="col-xs-12" style = "padding-top:20px;">
+    <div class = "content" id = "content">
+ 
+        </div>
+</div></div>
+
+<div class="row"><div class="col-xs-12" style = "padding-top:30px; text-align:center;padding-bottom:50px;">
+<hr><a href = "cidarlab.org">
+<img style = "display:inline-block; margin-left:auto;margin-right:auto;"src = "cidar.png"></a><br>
+<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.
+<p>Created by F. Carter Wheatley -- fwheat@bu.edu</p>
+</div></div>
+
+
+</html>
